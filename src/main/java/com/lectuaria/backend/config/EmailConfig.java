8c38ff41b7564b732f1ab89894424a2e39487f68
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-05-26 20:13:23 -0500

    fix: use SSL port 465 for Gmail SMTP

--- src/main/java/com/lectuaria/backend/config/EmailConfig.java
+++ src/main/java/com/lectuaria/backend/config/EmailConfig.java
@@ -42,13 +42,13 @@ public class EmailConfig {
         mailSender.setPassword(emailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.transport.protocol", "smtps");
         props.put("mail.smtp.auth", true);
-        props.put("mail.smtp.starttls.enable", true);
-        props.put("mail.smtp.starttls.required", true);
-        props.put("mail.smtp.connectiontimeout", "5000");
-        props.put("mail.smtp.timeout", "5000");
-        props.put("mail.smtp.writetimeout", "5000");
+        props.put("mail.smtp.ssl.enable", true);
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
         props.put("mail.debug", true);
 
         return mailSender;
